895b33a7456a74d53fbbfdf3c3abb42f259733ac
nzweb <nzweb@vk.com>
2023-02-16 17:12:42 +0300

    feat: add hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
+// burger
 $(".burger-mobile-menu ").on("click",".bar",function(){
     $(".mobile-menu").slideToggle();
     $(".bar").toggleClass('change');
     $(".mobile-menu li").slideRight();
 });
+
+// hero slider
+$('.hero-slider').slick({
+    prevArrow: $('.prev-arrow'),
+    nextArrow: $('.next-arrow'),
+    accessibility: false,
+    draggable: false,
+    dots: true
+});
+
+// custom checkbox
+$(".option").wrap("<div class='new'></div>");
+$('input[type="checkbox"]').after('<span class="check-box"></span>');
