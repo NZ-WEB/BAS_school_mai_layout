aeccba314e7381046d1f360cba8692477ababa2f
nzweb <nzweb@vk.com>
2023-02-23 13:48:01 +0300

    refactor: change styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,62 @@ $('.hero-slider').slick({
     autoplay: true
 });
 
+// phone mask
+//
+$(".contact-form__input[type=tel]").intlTelInput({
+// whether or not to allow the dropdown
+    allowDropdown: true,
+
+// if there is just a dial code in the input: remove it on blur, and re-add it on focus
+    autoHideDialCode: true,
+
+// add a placeholder in the input with an example number for the selected country
+    autoPlaceholder: "polite",
+
+// append menu to specified element
+    dropdownContainer: null,
+
+// don't display these countries
+    excludeCountries: [],
+
+// format the input value during initialisation and on setNumber
+    formatOnDisplay: true,
+
+// geoIp lookup function
+    geoIpLookup: null,
+
+// inject a hidden input with this name, and on submit, populate it with the result of getNumber
+    hiddenInput: "",
+
+// initial country
+    initialCountry: "ru",
+
+// localized country names e.g. { 'de': 'Deutschland' }
+    localizedCountries: null,
+
+// don't insert international dial codes
+    nationalMode: true,
+
+// display only these countries
+    onlyCountries: [],
+
+// number type to use for placeholders
+    placeholderNumberType: "MOBILE",
+
+// the countries at the top of the list. defaults to united states and united kingdom
+    preferredCountries: [ "ru", "am", "az", 'by', 'ua' ],
+
+// display the country dial code next to the selected flag so it's not part of the typed number
+    separateDialCode: false,
+
+// specify the path to the libphonenumber script to enable validation/formatting
+    utilsScript: "",
+
+    customPlaceholder: function(selectedCountryPlaceholder, selectedCountryData) {
+        return  selectedCountryPlaceholder + " *";
+    },
+});
+
 // form
 // $.validator.addClassRules({
 //     name: {
